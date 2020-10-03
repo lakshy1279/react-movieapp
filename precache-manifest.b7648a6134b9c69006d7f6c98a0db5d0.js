47d34c268a6077888b577352dbd0e627d0c36be1
lakshy pachory <luckyron1279@gmail.com>
2020-10-03 22:11:27 +0530

    Updates

---
+++ precache-manifest.b7648a6134b9c69006d7f6c98a0db5d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254fb7257303b72bb7486aeb78e0f383",
+    "url": "/react-movieapp/index.html"
+  },
+  {
+    "revision": "9cb20a83e3f91dcf196b",
+    "url": "/react-movieapp/static/css/main.57fbc47d.chunk.css"
+  },
+  {
+    "revision": "9c4b37b2e7d388e035df",
+    "url": "/react-movieapp/static/js/2.7569253b.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/react-movieapp/static/js/2.7569253b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb20a83e3f91dcf196b",
+    "url": "/react-movieapp/static/js/main.1593f45d.chunk.js"
+  },
+  {
+    "revision": "e6330089ca615ecdf4d2",
+    "url": "/react-movieapp/static/js/runtime-main.3a727013.js"
+  }
+]);
